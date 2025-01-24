5eebad453c6a4dc7dd0f748be813d6af39047c9e
legionJP <jjpsingh18@gmail.com>
2025-01-25 03:05:00 +0530

    adding the postgress project for docker container

---
+++ postgrsedocker/src/main/java/com/jspring6docker/postgrsedocker/PostgrsedockerApplication.java
@@ -0,0 +1,13 @@
+package com.jspring6docker.postgrsedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgrsedockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgrsedockerApplication.class, args);
+	}
+
+}
---
+++ postgrsedocker/src/test/java/com/jspring6docker/postgrsedocker/PostgrsedockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspring6docker.postgrsedocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgrsedockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
